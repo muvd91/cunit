99ae037fbb27ce5493d50b0e9d35b146e28dfa89
David Eliseo <david.muv91@gmail.com>
2017-06-23 23:10:37 -0500

    reduce global variables

--- src/cunit.c
+++ src/cunit.c
@@ -3,31 +3,32 @@
 #include<stdlib.h>
 #include "cunit.h"
 
-#define NO_REASONS 10
+#define NO_REASONS 100
 #define INT 1
 #define log printf("I got here!\n");
 
 struct utest {
     char *title;
     char *reasons[NO_REASONS];
+    char **reasons_ptr;
     int failed;
     struct utest *ptr;
-} dummy;
+};
+
+struct utest *head;
+struct utest *tail;
+int num_tests = 0;
 
 void print_reasons(char **, int);
 void free_utest(struct utest *);
 char * expected_actual_string(int, void *, void *);
 
-int num_tests = 0;
-char **reasons_ptr;
-
-struct utest *current;
 
 void
 print_results(void) {
-    struct utest *tmp = dummy.ptr;
+    struct utest *tmp = head;
     printf("Tests found: %d\n", num_tests);
-    if (!tmp)
+    if (!num_tests)
         return;
     while (tmp) {
         if (tmp->failed)
@@ -37,7 +38,7 @@ print_results(void) {
         tmp = tmp->ptr;
     }
     printf("\n");
-    tmp = dummy.ptr;
+    tmp = head;
     while (tmp) {
         if (tmp->failed > 0) {
             printf("Failed test: %s\n", tmp->title);
@@ -59,32 +60,41 @@ print_reasons(char **reasons, int number) {
 
 void
 test(const char *desc, void (*fn)()) {
-    struct utest *tmp = (struct utest *)malloc(sizeof(struct utest));
-    if (!num_tests)
-        dummy.ptr = current = tmp;
-    else
-        current->ptr = tmp;
-    current = tmp;
-    current->title = (char *)malloc(strlen(desc) + 1);
-    strcpy(current->title, desc);
-    current->failed = 0;
-    reasons_ptr = current->reasons;
-    current->ptr = NULL;
+    struct utest *new_test;
+    if (!num_tests) {
+        new_test = head = (struct utest *) malloc(sizeof(struct utest));
+        tail = head;
+    }
+    else {
+        new_test = (struct utest *) malloc(sizeof(struct utest));
+        tail->ptr = new_test;
+        tail = new_test;
+    }
+    tail->reasons_ptr = tail->reasons;
+
+    new_test->title = (char *)malloc(strlen(desc) + 1);
+    strcpy(new_test->title, desc);
+
+    new_test->failed = 0;
+    new_test->ptr = NULL;
+
     (*fn)();
     num_tests++;
 
+    tail = new_test;
 }
 
 int
 assert_equals_number(const char *reason, long double expected, long double actual) {
     if (expected == actual)
         return 1;
-    current->failed++;
-
+    tail->failed++;
     char *ex_ac = expected_actual_string(INT, &expected, &actual);
-    *reasons_ptr = (char *)malloc(strlen(reason) + strlen(ex_ac) + 1);
-    strcpy(*reasons_ptr, reason);
-    strcat(*reasons_ptr++, ex_ac);
+    *tail->reasons_ptr = (char *)malloc(strlen(reason) + strlen(ex_ac) + 1);
+
+    strcpy(*tail->reasons_ptr, reason);
+    strcat(*tail->reasons_ptr, ex_ac);
+    tail->reasons_ptr += 1;
     free(ex_ac);
     return 0;
 }
